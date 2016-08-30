8c58caf148746b762cd3c997feac65d2b44ffb9f
Kevin <shift8web@3057983.no-reply.drupal.org>
2016-08-30 10:32:29 -0400

    Latest changes to admin settings interface

--- shift8-fbfeed.php
+++ shift8-fbfeed.php
@@ -17,8 +17,10 @@ add_action('admin_menu', 'shift8_fbfeed_create_menu');
 
 function shift8_fbfeed_create_menu() {
 	//create new top-level menu
-	add_menu_page('Shift8 FB Feed Settings', 'Shift8', 'administrator', __FILE__, 'shift8_main_page' , 'dashicons-building' );
-	add_submenu_page(__FILE__, 'Facebook Feed Settings', 'Facebook Feed Settings', 'manage_options', __FILE__.'/custom', 'shift8_fbfeed_settings_page');
+	if ( empty ( $GLOBALS['admin_page_hooks']['shift8-settings'] ) ) {
+		add_menu_page('Shift8 Settings', 'Shift8', 'administrator', 'shift8-settings', 'shift8_main_page' , 'dashicons-building' );
+	}
+	add_submenu_page('shift8-settings', 'Shift8 Settings', 'Facebook Feed Settings', 'manage_options', __FILE__.'/custom', 'shift8_fbfeed_settings_page');
 	//call register settings function
 	add_action( 'admin_init', 'register_shift8_fbfeed_settings' );
 }
